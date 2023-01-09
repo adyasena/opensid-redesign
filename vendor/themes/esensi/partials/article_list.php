c57878842910b87c6f4c3d616a0be79247f77a9f
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-09 14:28:31 +0700

    Revert "Update article_list.php"
    This reverts commit 1a8ffddd7866dac8b814c82d82afca9f69edc942.

--- vendor/themes/esensi/partials/article_list.php
+++ vendor/themes/esensi/partials/article_list.php
@@ -7,13 +7,13 @@
   gambar_desa($desa['logo']);
 ?>
 
-<div class="bg-white shadow rounded-lg p-3 lg:p-5 border overflow-auto" style="width: 400px; height: 400px;">
-  <figure class="<?php $image === gambar_desa($desa['logo']) and print('') ?>" style="width: 100%; height: 100%; position: relative;">
-    <img src="<?= $image ?>" alt="<?= $post['judul'] ?>" class="<?php $image !== gambar_desa($desa['logo']) and print('w-full h-full') ?> max-w-full mx-auto bg-opacity-60" style=" object-fit: cover;" >
-    <div class="absolute bg-gray-800 bg-opacity-60 top-0 left-0 right-0 h-full"></div>
+<div class="max-w-full w-full bg-white shadow rounded-lg p-3 lg:p-5 border overflow-auto">
+  <figure class="h-auto lg:h-32 w-1/3 float-left pr-3 pb-3 <?php $image === gambar_desa($desa['logo']) and print('lg:h-auto') ?>">
+    <img src="<?= $image ?>" alt="<?= $post['judul'] ?>" class="<?php $image !== gambar_desa($desa['logo']) and print('lg:h-32 w-full object-cover object-center') ?> max-w-full mx-auto h-auto">
   </figure>
   <div class="space-y-3">
     <a href="<?= $url ?>" class="text-h5 hover:text-primary-100"><?= $post['judul'] ?></a>
+    <p class="line-clamp-4"><?= $abstract ?></p>
     <ul class="inline-flex gap-x-5 gap-y-3 text-xs lg:text-sm text-gray-500 flex-wrap">
       <li><i class="fas fa-calendar-alt mr-1 text-primary-100"></i> <?= tgl_indo($post['tgl_upload']) ?></li>
       <li><i class="fas fa-user mr-1 text-primary-100"></i> <?= $post['owner'] ?></li>
