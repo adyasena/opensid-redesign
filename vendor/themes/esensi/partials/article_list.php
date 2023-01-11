d10ea6488fda6400e1e92931619769a874812056
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-11 10:19:25 +0700

    update article list UI

--- vendor/themes/esensi/partials/article_list.php
+++ vendor/themes/esensi/partials/article_list.php
@@ -7,19 +7,20 @@
   gambar_desa($desa['logo']);
 ?>
 
-<div class="max-w-full w-full lg:pb-5 overflow-auto">
-  <figure class="h-32 lg:h-40 w-1/2 lg:w-1/3 float-left pr-3 relative <?php $image === gambar_desa($desa['logo']) ?>">
+<div class="max-w-full w-full pb-2 lg:pb-5 overflow-auto" style="border-bottom: 1px solid #ccc;">
+  <figure class="h-20 lg:h-32 w-1/3 float-left relative <?php $image === gambar_desa($desa['logo']) ?>" style="padding-right: 0.5rem;">
     <img src="<?= $image ?>" style="position: relative; height: 100%; top: 0; left:0;" 
       alt="<?= $post['judul'] ?>" class="<?php $image !== gambar_desa($desa['logo']) and print('w-full object-cover object-center') ?> max-w-full mx-auto h-auto">
-      <div class="text-xs lg:text-sm text-white p-2" style="z-index:1000; background-color:#8a0090; position: absolute; opacity: 0.85; bottom:0; left:0;">
+      <div class="text-xs lg:text-sm text-white p-1" style="lg:padding: 0.25rem; z-index:1000; background-color:#8a0090; position: absolute; opacity: 0.85; bottom:0; left:0;">
       <?= tgl_indo($post['tgl_upload']) ?> </div>
   </figure>
-  <div class="mb-9 w-full">
-    <a href="<?= $url ?>" class="text-h5 text-primary-200 hover:text-primary-100 my-2"><?= $post['judul'] ?></a>
-    <p class="line-clamp-4" style="margin-bottom: 4px;"><?= $abstract ?></p>
-    <a href="<?= $url ?>" class="text-md text-primary-200 hover:text-primary-100">
-      Lihat Selengkapnya
-      <i class="fas fa-chevron-right ml-1"></i>
-    </a>
+  <a href="<?= $url ?>" class="text-h5 text-primary-200 hover:text-primary-100 my-2"><?= $post['judul'] ?></a>
+  <div class="flex flex-col">
+  <p class="line-clamp-4" style="margin-bottom: 4px; -webkit-line-clamp: 3;"><?= $abstract ?></p>
+  <a href="<?= $url ?>" class="text-md text-primary-200 hover:text-primary-100">
+    Lihat Selengkapnya
+  <i class="fas fa-chevron-right ml-1"></i>
+  </a>
   </div>
+  
 </div>
