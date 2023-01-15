08ff92d1a4e7cd735f18da3068084a13f4ddedc7
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-15 17:14:30 +0700

    update footer color

--- vendor/themes/manggis/commons/footer.php
+++ vendor/themes/manggis/commons/footer.php
@@ -42,10 +42,10 @@
 <?php $this->load->view($folder_themes .'/commons/back_to_top') ?>
 
 <footer class="container mx-auto lg:px-5 px-3 pt-5 footer">
-  <div class="bg-zinc-700 text-white py-5 px-5 rounded-t-xl text-sm flex flex-col gap-3 lg:flex-row justify-between items-center text-center lg:text-left">
+  <div class="bg-neutral-800 text-white py-5 px-5 rounded-t-xl text-sm flex flex-col gap-3 lg:flex-row justify-between items-center text-center lg:text-left">
     <span class="space-y-2">
       <p>Hak cipta situs &copy; <?= date('Y') ?> - <?= NAMA_DESA ?></p>
-      <p><a href="https://www.trivusi.web.id" class="underline decoration-pink-500 underline-offset-1 decoration-2" target="_blank" rel="noopener">Manggis v1.0</a> - <a href="https://opensid.my.id" class="underline decoration-green-500 underline-offset-1 decoration-2" target="_blank" rel="noopener">OpenSID <?= ambilVersi() ?></a>
+      <p><a href="https://www.trivusi.web.id" class="underline decoration-primary underline-offset-1 decoration-2" target="_blank" rel="noopener">Manggis v1.0</a> - <a href="https://opensid.my.id" class="underline decoration-accent underline-offset-1 decoration-2" target="_blank" rel="noopener">OpenSID <?= ambilVersi() ?></a>
       </p>
     </span>
     <ul class="space-x-1">
