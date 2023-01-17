975a35325cdcc274a0cc396e06b23c2417d6b89b
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-17 14:02:40 +0700

    update menu

--- vendor/themes/manggis/commons/main_menu.php
+++ vendor/themes/manggis/commons/main_menu.php
@@ -7,40 +7,35 @@
     </li>
     <?php if($menu_atas) : ?>
       <?php foreach($menu_atas as $menu) : ?>
-        <?php $has_dropdown = count($menu['submenu']) > 0 ?>
-        <li class="inline-block relative" <?php $has_dropdown and print('x-data="{dropdown: false}"') ?>>
-
-          <?php $menu_link = $has_dropdown ? '#!' : $menu['link'] ?>
-
-          <a href="<?= $menu_link ?>"
-            class="p-3 inline-block hover:bg-primary-200"
-            @mouseover="dropdown = true"
-            @mouseleave="dropdown = false"
-            @click="dropdown = !dropdown"
-            <?php $has_dropdown and print('aria-expanded="false" aria-haspopup="true"') ?>
-            >
-            <?= $menu['nama'] ?>
-            
-            <?php if($has_dropdown) : ?>
-              <i class="fas fa-chevron-down text-xs ml-1 inline-block transition duration-300" :class="{'transform rotate-180': dropdown}"></i>
-            <?php endif ?>
-          </a>
-
-          <?php if($has_dropdown) : ?>
-            <ul
-              class="absolute top-full w-64 bg-white text-gray-700 shadow-lg invisible transform transition duration-200 origin-top" 
-              :class="{'opacity-0 invisible z-[-10] scale-y-50': !dropdown, 'opacity-100 visible z-[9999] scale-y-100': dropdown}"
-              x-transition
+        <?php if (substr($menu['link'], -4) !== "peta") : ?>
+          <?php $has_dropdown = count($menu['submenu']) > 0 ?>
+          <li class="inline-block relative" <?php $has_dropdown and print('x-data="{dropdown: false}"') ?>>
+            <?php $menu_link = $has_dropdown ? '#!' : $menu['link'] ?>
+            <a href="<?= $menu_link ?>"
+              class="p-3 inline-block hover:bg-primary-200"
               @mouseover="dropdown = true"
-              @mouseleave="dropdown = false">
-
-              <?php foreach($menu['submenu'] as $submenu) : ?>
-                <li><a href="<?= $submenu['link'] ?>" class="block py-3 pl-5 pr-4 hover:bg-primary-200 hover:text-white"><?= $submenu['nama'] ?></a></li>
-              <?php endforeach ?>
-              
-            </ul>
-          <?php endif ?>
-        </li>
+              @mouseleave="dropdown = false"
+              @click="dropdown = !dropdown"
+              <?php $has_dropdown and print('aria-expanded="false" aria-haspopup="true"') ?>>
+              <?= $menu['nama'] ?>
+              <?php if($has_dropdown) : ?>
+                <i class="fas fa-chevron-down text-xs ml-1 inline-block transition duration-300" :class="{'transform rotate-180': dropdown}"></i>
+              <?php endif ?>
+            </a>
+            <?php if($has_dropdown) : ?>
+              <ul
+                class="absolute top-full w-64 bg-white text-gray-700 shadow-lg invisible transform transition duration-200 origin-top" 
+                :class="{'opacity-0 invisible z-[-10] scale-y-50': !dropdown, 'opacity-100 visible z-[9999] scale-y-100': dropdown}"
+                x-transition
+                @mouseover="dropdown = true"
+                @mouseleave="dropdown = false">
+                <?php foreach($menu['submenu'] as $submenu) : ?>
+                  <li><a href="<?= $submenu['link'] ?>" class="block py-3 pl-5 pr-4 hover:bg-primary-200 hover:text-white"><?= $submenu['nama'] ?></a></li>
+                <?php endforeach ?> 
+              </ul>
+            <?php endif ?>
+          </li>
+        <?php endif ?>
       <?php endforeach ?>
     <?php endif ?>
   </ul>
--- vendor/themes/manggis/commons/mobile_menu.php
+++ vendor/themes/manggis/commons/mobile_menu.php
@@ -19,35 +19,31 @@
     class="divide-y divide-primary-200">
     <?php if($menu_atas) : ?>
       <?php foreach($menu_atas as $menu) : ?>
-        <?php $has_dropdown = count($menu['submenu']) > 0 ?>
-        <li class="block relative" <?php $has_dropdown and print('x-data="{dropdown: false}"') ?>>
-
-          <?php $menu_link = $has_dropdown ? '#!' : $menu['link'] ?>
-
-          <a href="<?= $menu_link ?>"
-            class="p-3 block hover:bg-secondary-100"
-            @click="dropdown = !dropdown">
-            <?= $menu['nama'] ?>
-            
-            <?php if($has_dropdown) : ?>
-              <i class="fas fa-chevron-down text-xs ml-1 inline-block transition duration-300" :class="{'transform rotate-180': dropdown}"></i>
-            <?php endif ?>
-          </a>
-
-          <?php if($has_dropdown) : ?>
-            <ul
-              class="divide-y divide-primary-200"
-              :class="{'opacity-0 invisible z-[-10] scale-y-75 h-0': !dropdown, 'opacity-100 visible z-30 scale-y-100 h-auto': dropdown}"
-              x-transition.opacity
+        <?php if (substr($menu['link'], -4) !== "peta") : ?>
+          <?php $has_dropdown = count($menu['submenu']) > 0 ?>
+          <li class="block relative" <?php $has_dropdown and print('x-data="{dropdown: false}"') ?>>
+            <?php $menu_link = $has_dropdown ? '#!' : $menu['link'] ?>
+            <a href="<?= $menu_link ?>"
+              class="p-3 block hover:bg-secondary-100"
               @click="dropdown = !dropdown">
-
-              <?php foreach($menu['submenu'] as $submenu) : ?>
-                <li @click="dropdown = false"><a href="<?= $submenu['link'] ?>" class="block py-3 pl-5 pr-4 hover:bg-primary-200 hover:text-white"><?= $submenu['nama'] ?></a></li>
-              <?php endforeach ?>
-              
-            </ul>
-          <?php endif ?>
-        </li>
+              <?= $menu['nama'] ?>            
+              <?php if($has_dropdown) : ?>
+                <i class="fas fa-chevron-down text-xs ml-1 inline-block transition duration-300" :class="{'transform rotate-180': dropdown}"></i>
+              <?php endif ?>
+            </a>
+            <?php if($has_dropdown) : ?>
+              <ul
+                class="divide-y divide-primary-200"
+                :class="{'opacity-0 invisible z-[-10] scale-y-75 h-0': !dropdown, 'opacity-100 visible z-30 scale-y-100 h-auto': dropdown}"
+                x-transition.opacity
+                @click="dropdown = !dropdown">
+                <?php foreach($menu['submenu'] as $submenu) : ?>
+                  <li @click="dropdown = false"><a href="<?= $submenu['link'] ?>" class="block py-3 pl-5 pr-4 hover:bg-primary-200 hover:text-white"><?= $submenu['nama'] ?></a></li>
+                <?php endforeach ?>             
+              </ul>
+            <?php endif ?>
+          </li>
+        <?php endif ?>
       <?php endforeach ?>
     <?php endif ?>
   </ul>
